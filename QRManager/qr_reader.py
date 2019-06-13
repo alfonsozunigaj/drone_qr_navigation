3f89d0ed380207d3437119cac8c51a2de867944e
MargaritaEstevez <mmestevez@miuandes.cl>
2019-06-13 10:35:21 -0400

    instruction compatible program. added state showing console logs.

--- QRManager/qr_reader.py
+++ QRManager/qr_reader.py
@@ -4,5 +4,6 @@ from pyzbar.pyzbar import decode
 def read_qr_code(input_image):
     decoded_image = decode(input_image)
     if decoded_image:
+        print("[QR DETECTION]", decoded_image)
         return decoded_image[0][0].decode("utf-8")
     return None
--- drone_vision_no_gui.py
+++ drone_vision_no_gui.py
@@ -10,14 +10,6 @@ def has_content(something):
     return something is not None
 
 
-def translate_move(values_list):
-    return {'roll': 0,
-            'pitch': 0,
-            'yaw': 0,
-            'vertical_movement': 0,
-            "duration": 0}
-
-
 class UserVision:
     def __init__(self, vision, instructions, drone):
         self.index = 0
@@ -25,73 +17,61 @@ class UserVision:
         self.instructions = instructions
         self.drone = drone
 
-    def save_pictures(self, args):
-
-        img = self.vision.get_latest_valid_picture()
-
+    def save_pictures(self, img):
         if has_content(img):
             self.user_code_after_vision_opened(img)
             filename = "images/test_image_%06d.png" % self.index
+            print("[SAVE] saving image: ", filename)
             cv2.imwrite(filename, img)
             self.index += 1
-            # print(self.index)
 
     def user_code_after_vision_opened(self, args):
         img = self.vision.get_latest_valid_picture()
-        # self.save_pictures(img)
-
         if has_content(img):
             instruction = qr_manager.read_qr_code(img)
-            print(instruction)
-            self.translate_instruction(instruction)
+            if instruction:
+                self.save_pictures(img)
+            self.perform_instruction(instruction)
 
-    def translate_instruction(self, instruction):
+    def perform_instruction(self, instruction):
+        print("[PERFORM] instruction:", instruction)
         if not has_content(instruction):
             return
-        """
-        if instruction in self.instructions:
-            values = self.instructions[instruction]
-            m = translate_move(values)
-
-            self.drone.fly_direct(roll=m['roll'],
-                                  pitch=m['pitch'],
-                                  yaw=m['yaw'],
-                                  vertical_movement=m['vertical_movement'],
-                                  duration=m['duration'])
-                                  """
-        if instruction == "A": # adelante
-            self.drone.fly_direct(roll=0, pitch=30, yaw=0, vertical_movement=0,
-                                  duration=2)
-        elif instruction == "B": # girar
-            self.drone.fly_direct(roll=50, pitch=0, yaw=0, vertical_movement=0,
-                                  duration=1)
-        elif instruction == "take_off":
-
+        if instruction == "take_off":
             self.drone.safe_takeoff(1)
+
         elif instruction == "land":
-            self.drone.safe_land(5)
-            self.vision.close_video()
-            self.drone.disconnect()
+            self.perform_landing()
+
+        elif instruction in self.instructions:
+            self.follow_instructions(instruction)
+
+    def follow_instructions(self, instruction):
+        for movement in self.instructions[instruction]:
+            self.drone.fly_direct(roll=movement['roll'],
+                                  pitch=movement['pitch'],
+                                  yaw=movement['yaw'],
+                                  vertical_movement=movement['v_move'],
+                                  duration=movement['duration'])
+
+    def perform_landing(self):
+        self.drone.safe_land(5)
+        self.vision.close_video()
+        self.drone.disconnect()
 
 
 if __name__ == '__main__':
 
-    mamboAddr = "d0:3a:4d:78:e6:36"
+    INSTRUCTIONS = file_manager.get_navigation_instructions_from_json()
+    print ("[INSTRUCTION] Dictionary created")
+    MAMBO_ADDRESS = "d0:3a:4d:78:e6:36"
 
-    mambo = Mambo(mamboAddr, use_wifi=True)
-    print("trying to connect to mambo now")
-    SUCCESS = mambo.connect(num_retries=20)
-    print("connected: %s" % SUCCESS)
+    mambo = Mambo(MAMBO_ADDRESS, use_wifi=True)
+    mambo_connected = mambo.connect(num_retries=20)
+    print("[MAMBO]connected: %s" % mambo_connected)
     mambo.smart_sleep(1)
-    mambo.ask_for_state_update()
-    mambo.smart_sleep(1)
-    # bebop = Bebop(drone_type="Bebop")
 
-    # SUCCESS = bebop.connect(5)
-    INSTRUCTIONS = file_manager.get_navigation_instructions()
-    print (INSTRUCTIONS)
-
-    if SUCCESS:
+    if mambo_connected:
         vision = DroneVision(mambo, is_bebop=False, buffer_size=30)
         userVision = UserVision(vision, INSTRUCTIONS, mambo)
 
@@ -99,8 +79,8 @@ if __name__ == '__main__':
             userVision.user_code_after_vision_opened,
             user_callback_args=None)
         success = vision.open_video()
-        print("Success in opening vision is %s" % success)
+        print("[VISION] Success in opening vision is %s" % success)
         mambo.smart_sleep(3)
 
     else:
-        print("Error connecting to bebop.  Retry")
+        print("[VISION]Error connecting to bebop.  Retry")
