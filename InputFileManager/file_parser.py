90dab3b70df0b007a76e2f4d86a71f0fce31a0d0
MargaritaEstevez <mmestevez@miuandes.cl>
2019-06-13 10:33:52 -0400

    csv compatible with instruction translation.

--- InputFileManager/file_parser.py
+++ InputFileManager/file_parser.py
@@ -8,12 +8,22 @@ def get_navigation_instructions_from_csv(*args):
     csv_reader = csv.reader(csv_file, delimiter=',', quotechar='"')
     instructions = {}
     for row in csv_reader:
-        instructions[row[0]] = list(map(int, row[1:]))
+        instructions[row[0]] = [{"roll": row[1],
+                                 "pitch": row[2],
+                                 "yaw": row[3],
+                                 "v_move": row[4],
+                                 "duration": row[5]
+                                 }]
     return instructions
 
 
 def get_navigation_instructions_from_json(*args):
-    file_name = "instructions.json" if not args else args[0]
+    file_name = "InputFileManager/instructions.json" if not args else args[0]
     json_file = open(file_name, mode="r", encoding="utf-8")
     instructions = json.loads(json_file.read())
     return instructions
+
+
+if __name__ == '__main__':
+    print (get_navigation_instructions_from_csv())
+    print (get_navigation_instructions_from_json())
